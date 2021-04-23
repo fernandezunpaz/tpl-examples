d992a54e25b5b6b8188dd4659526d5291e89fe73
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-22 22:27:48 -0300

    First example

---
+++ 01-TwoTasks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_TwoTasks
+{
+    class Program
+    {
+        static void MethodA()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("This is the MethodA");
+        }
+
+        static void MethodB()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("This is the MethodB");
+        }
+
+        static void Main(string[] args)
+        {
+            Task taskA = Task.Factory.StartNew(MethodA);
+            Task taskB = Task.Factory.StartNew(MethodB);
+
+            //Another Task execution
+            //Task taskA = new Task(MethodA);
+            //taskA.Start();
+            //Task taskB = new Task(MethodB);
+            //taskB.Start();
+
+            //Another Task execution
+            //Parallel.Invoke(new Action[] { MethodA, MethodB } );
+            
+            //...
+            //...
+            //...
+
+            Console.ReadKey();
+        }
+    }
+}
