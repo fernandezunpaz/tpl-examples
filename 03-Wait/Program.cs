bbbc1a090bfbd21f8bfd69f3d0ad8ec8b5eedba9
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-22 23:11:56 -0300

    Wait example

---
+++ 03-Wait/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_Wait
+{
+    class Program
+    {
+
+        static void MethodA()
+        {
+            Thread.SpinWait(int.MaxValue);
+            Console.WriteLine("This is the MethodA");
+        }
+
+        static void MethodB()
+        {
+            Thread.SpinWait(int.MaxValue / 2);
+            Console.WriteLine("This is the MethodB");
+        }
+
+        static void Main(string[] args)
+        {
+            Task taskA = Task.Factory.StartNew(MethodA);
+
+            Console.WriteLine("Se inicia la espera de la taskA");
+            taskA.Wait();
+            Console.WriteLine("Finalizo la espera de la taskA");
+
+            Task taskB = Task.Factory.StartNew(MethodB);
+
+            Console.WriteLine("Se inicia la espera de la taskB");
+            taskB.Wait();
+            Console.WriteLine("Finalizo la espera de la taskB");
+
+            Console.WriteLine("taskA id = {0}", taskA.Id);
+            Console.WriteLine("taskB id = {0}", taskB.Id);
+
+            Console.ReadLine();
+        }
+    }
+}
