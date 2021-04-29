3b3c0d2fcf708a0e70c53c699086d9b29391a550
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-29 18:56:05 -0300

    Harvest results example

---
+++ 06-ReturningResults/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ReturningResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task<int> t1 = new Task<int>(() =>
+            {
+                return 42;
+            });
+
+            t1.Start();
+
+            t1.Wait();
+            int result = t1.Result;
+            Console.WriteLine("El resultado de t1 es: {0}", result.ToString());
+
+        }
+    }
+}
