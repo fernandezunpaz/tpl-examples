c37dc3821bf5187db8d644370018abf511b71e27
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-29 19:29:40 -0300

    ContinueWith example

---
+++ 07-ContinueWith/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07_ContinueWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int stock = 100;
+
+            Task<int> t1 = new Task<int>( (arg) => {
+                //Consulto base de datos de inventario del almacen B
+                Thread.Sleep(4000);
+                int stockLocal = (int)arg;
+                stockLocal += 5;
+                return stockLocal;
+            }, stock);
+
+            t1.Start();
+
+            Task<int> t2 = t1.ContinueWith( (predecesor) => {
+                //Consulto base de datos de inventario del almacen X
+                int stockLocal = predecesor.Result;
+                stockLocal += 10;
+                return stockLocal;
+            });
+            
+            t2.Wait();
+
+            Console.WriteLine("El stock es {0}", t2.Result);
+        }
+    }
+}
