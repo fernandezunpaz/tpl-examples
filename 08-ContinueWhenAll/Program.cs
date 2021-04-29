87bcca5060358f4c6c9f96fac98bf1bdcddecf89
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-29 19:42:01 -0300

    ContinueWhenAll example

---
+++ 08-ContinueWhenAll/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ContinueWhenAll
+{
+    class Program
+    {
+        static int PerformCalculation() { return 42; }
+
+        static void Main(string[] args)
+        {
+            var TaskA = new Task<int>(() =>
+            {
+                Console.WriteLine("TaskA started.");
+                return PerformCalculation();
+            });
+            var TaskB = new Task<int>(() =>
+            {
+                Console.WriteLine("TaskB started.");
+                return PerformCalculation();
+            });
+
+            var total = Task.Factory.ContinueWhenAll(new Task<int>[] { TaskA, TaskB },
+                (tasks) => Console.WriteLine(
+                    "Total = {0}", tasks[0].Result + tasks[1].Result));
+
+            TaskA.Start();
+            TaskB.Start();
+
+            Task.WaitAll(TaskA, TaskB);
+            total.Wait();
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
